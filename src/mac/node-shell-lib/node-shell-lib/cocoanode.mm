bfdd4fc19ec32fb5019451f530c973237142ea20
Joel Brandt <joelrbrandt@gmail.com>
2012-05-30 13:56:44 -0700

    working on getting brackets stuff integrated

--- src/mac/node-shell-lib/node-shell-lib/cocoanode.mm
+++ src/mac/node-shell-lib/node-shell-lib/cocoanode.mm
@@ -12,23 +12,69 @@
 #include <node.h>
 #include <v8.h>
 
-v8::Handle<v8::Value> Method(const v8::Arguments& args);
+v8::Handle<v8::Value> getSomeFiles(const v8::Arguments& args);
 void init(v8::Handle<v8::Object> target);
+NSString* NSArrayToJSONString(NSArray* array);
+NSString* showOpenDialog();
 
-v8::Handle<v8::Value> Method(const v8::Arguments& args) {
+
+v8::Handle<v8::Value> getSomeFiles(const v8::Arguments& args) {
     v8::HandleScope scope;
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert addButtonWithTitle:@"OK"];
-    [alert addButtonWithTitle:@"Cancel"];
-    [alert setMessageText:@"Delete the record?"];
-    [alert setInformativeText:@"Deleted records cannot be restored."];
-    [alert setAlertStyle:NSWarningAlertStyle];
-    [alert runModal];
-    return scope.Close(v8::String::New("world shmirld"));
+    NSString* result = showOpenDialog();
+    return scope.Close(v8::String::New([result UTF8String]));
 }
 
 void init(v8::Handle<v8::Object> target) {
-    target->Set(v8::String::NewSymbol("hello"),
-                v8::FunctionTemplate::New(Method)->GetFunction());
+    target->Set(v8::String::NewSymbol("getSomeFiles"),
+                v8::FunctionTemplate::New(getSomeFiles)->GetFunction());
+}
+
+NSString* NSArrayToJSONString(NSArray* array) {        
+    int numItems = [array count];
+    NSString* escapedStr = @"";
+    NSMutableString* result = [[[NSMutableString alloc] initWithString:@"["] autorelease];
+    NSString* item;
+    
+    for (int i = 0; i < numItems; i++)
+    {
+        [result appendString:@"\""];
+        
+        item = [array objectAtIndex:i];
+        escapedStr = item; //EscapeJSONString(item, escapedStr);
+        
+        [result appendString:escapedStr];
+        [result appendString:@"\""];
+        
+        
+        if (i < numItems - 1)
+            [result appendString:@", "];
+    }
+    [result appendString:@"]"];
+    
+    return result;
+}
+
+
+NSString* showOpenDialog() {
+    NSString* result = @"";  
+    NSArray* allowedFileTypes = nil;
+        
+    // Initialize the dialog
+    NSOpenPanel* openPanel = [NSOpenPanel openPanel];
+    [openPanel setCanChooseFiles:YES];
+    [openPanel setCanChooseDirectories:NO];
+    [openPanel setCanCreateDirectories:YES];
+    [openPanel setAllowsMultipleSelection:NO];
+    [openPanel setTitle: @"Exciting times!"];
+    
+    [openPanel setAllowedFileTypes:allowedFileTypes];
+    
+    if ([openPanel runModal] == NSOKButton)
+    {
+        NSArray *f = [openPanel filenames];
+        result = NSArrayToJSONString(f);
+    }
+    
+    return result;
 }
 NODE_MODULE(cocoanode, init)
---
+++ src/mac/node-shell-lib/node-shell-lib/server/server.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+/*jslint sloppy: true, vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global require, process, setInterval, setTimeout, clearTimeout, __dirname */
+
+var config = require('./config');
+var clientProxy = require('./clientProxy');
+
+var SETUP_TIMEOUT = 5000; // wait up to 5 seconds for http and ws server to start
+
+function setupHttpAndWebSocketServers(callback, timeout) {
+    var connect = require('connect');
+    var timeoutTimer = null;
+    var httpServer = null;
+    var httpServerSocket = null;
+
+    if (timeout) {
+        timeoutTimer = setTimeout(function () { callback("ERR_TIMEOUT", null); }, timeout);
+    }
+
+    httpServer = connect()
+        .use(connect.static(config.clientPath))
+        .use(function (req, res) {
+            res.end('hello world\n');
+        });
+
+    console.log('starting http server');
+    httpServerSocket = httpServer.listen(0, '127.0.0.1', function () {
+        console.log('http server listening, starting wss');
+        var WebSocketServer = require('ws').Server;
+        var wsServer = null;
+        if (httpServerSocket !== null) {
+            wsServer = new WebSocketServer({server: httpServerSocket});
+            wsServer.on('connection', function (ws) { console.log('got a connection'); clientProxy.setWebsocket(ws); });
+            console.log('wss created');
+            if (timeoutTimer) {
+                clearTimeout(timeoutTimer);
+            }
+            callback(null, {httpServer : httpServer, wsServer : wsServer, port : httpServerSocket.address().port });
+        } else { // httpServerSocket is null -- this shouldn't happen, because if we didn't get a socket we wouldn't have called this callback
+            if (timeoutTimer) {
+                clearTimeout(timeoutTimer);
+            }
+            callback("ERR_UNKNOWN");
+        }
+    });
+}
+
+
+function start() {
+    setupHttpAndWebSocketServers(function (err, servers) {
+        if (err) {
+            console.error("Got error starting servers: " + err);
+        } else {
+            console.log("serving on port: " + servers.port);
+        }
+    }, SETUP_TIMEOUT);
+}
+
+// change console.log and console.info to print to stderr (like console.warn and console.error do)
+// other things print to stdout, so we still need to be robust to this in our messaging protocol
+// but this avoids lots of needless messages sent to the app shell
+console.log = console.info = console.warn;
+console.log('starting node in dir: ' + __dirname);
+start();
